290eadd09dcbf638a5301c80c109317fa1149f8b
dmitry.astu@gmail.com <dmitry.astu@gmail.com@0b333480-fe57-fea6-b96e-3b5c4f84aef2>
2010-12-23 14:04:45 +0000

    Undo/redo fix, action set fix

--- trunk/snippets/com.onpositive.richtexteditor.snippets.browser/src/com/onpositive/richtexteditor/snippets/browser/View.java
+++ trunk/snippets/com.onpositive.richtexteditor.snippets.browser/src/com/onpositive/richtexteditor/snippets/browser/View.java
@@ -1,5 +1,9 @@
 package com.onpositive.richtexteditor.snippets.browser;
 
+import org.eclipse.core.commands.IHandler;
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.commands.ActionHandler;
+import org.eclipse.jface.text.TextViewer;
 import org.eclipse.jface.viewers.IStructuredContentProvider;
 import org.eclipse.jface.viewers.ITableLabelProvider;
 import org.eclipse.jface.viewers.LabelProvider;
@@ -13,18 +17,21 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.ISharedImages;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.handlers.IHandlerService;
 import org.eclipse.ui.part.ViewPart;
 
 import com.onpositive.richtext.model.IRichDocumentListener;
 import com.onpositive.richtext.model.RichDocumentChange;
 import com.onpositive.richtext.model.meta.DocumentEvent;
 import com.onpositive.richtexteditor.viewer.RichTextViewer;
+import com.onpositive.richtexteditor.viewer.undo.RichViewerUndoManager;
 
 public class View extends ViewPart {
+	private static final int UNDO_CACHE_SIZE = 100;
 	public static final String ID = "com.onpositive.richtexteditor.snippets.browser.view";
 	private static final int MIN_UPDATE_INTERVAL = 2000; //2 sec
 
-	private RichTextViewer viewer;
+	private RichTextViewer richTextViewer;
 	private long lastUpdateTime = -1;
 	private Boolean updateScheduled = false;
 	
@@ -37,19 +44,23 @@ public class View extends ViewPart {
 	public void createPartControl(Composite parent) {
 		
 		parent.setLayout(new GridLayout(2,true));
-		viewer = new RichTextViewer(parent, SWT.BORDER);
+		richTextViewer = new RichTextViewer(parent, SWT.BORDER);
 		GridData gridData = new GridData(GridData.FILL_BOTH);
 		gridData.horizontalSpan = 1;
 		gridData.minimumWidth = 200;
 		gridData.minimumHeight = 400;
-		viewer.getControl().setLayoutData(gridData);		
+		richTextViewer.getControl().setLayoutData(gridData);		
+		RichViewerUndoManager undoManager = new RichViewerUndoManager(UNDO_CACHE_SIZE);
+		undoManager.connect(richTextViewer);
+		richTextViewer.setUndoManager(undoManager);
 		final Browser browser = new Browser(parent, SWT.BORDER );
 		GridData gridData1 = new GridData(GridData.FILL_BOTH);
 		gridData1.horizontalSpan = 1;
 		gridData1.minimumWidth = 200;
 		gridData1.minimumHeight = 400;
-		browser.setLayoutData(gridData1);				
-		viewer.addRichDocumentListener(new IRichDocumentListener(){
+		browser.setLayoutData(gridData1);		
+		registerHandlers(); 
+		richTextViewer.addRichDocumentListener(new IRichDocumentListener(){
 
 			public void documentAboutToBeChanged(DocumentEvent event) {				
 			}
@@ -63,7 +74,7 @@ public class View extends ViewPart {
 					{
 						if (lastUpdateTime == -1 || current - lastUpdateTime < MIN_UPDATE_INTERVAL)
 						{
-							String s =viewer.getLayerManager().getSerializedString();
+							String s =richTextViewer.getLayerManager().getSerializedString();
 							browser.setText(s);
 							lastUpdateTime = current;
 						}
@@ -75,7 +86,7 @@ public class View extends ViewPart {
 								@Override
 								public void run()
 								{
-									String s = viewer.getLayerManager().getSerializedString();
+									String s = richTextViewer.getLayerManager().getSerializedString();
 									browser.setText(s);
 									updateScheduled = false;
 									lastUpdateTime = System.currentTimeMillis();
@@ -90,10 +101,40 @@ public class View extends ViewPart {
 		});		
 	}
 
+	protected void registerHandlers()
+	{
+		Action undoAction = new Action()
+		{
+			@Override
+			public void run()
+			{
+				richTextViewer.doOperation(TextViewer.UNDO);
+			}
+		};
+		undoAction.setId("com.onpositive.richtexteditor.snippets.browser.undo");
+		undoAction.setActionDefinitionId("com.onpositive.richtexteditor.snippets.browser.undo");
+		ActionHandler undoHandler = new ActionHandler(undoAction);
+		((IHandlerService)getSite().getService(IHandlerService.class)).
+		activateHandler("com.onpositive.richtexteditor.snippets.browser.undo", (IHandler)undoHandler);
+		Action redoAction = new Action()
+		{
+			@Override
+			public void run()
+			{
+				richTextViewer.doOperation(TextViewer.REDO);
+			}
+		};
+		redoAction.setId("com.onpositive.richtexteditor.snippets.browser.redo");
+		redoAction.setActionDefinitionId("com.onpositive.richtexteditor.snippets.browser.redo");
+		ActionHandler redoHandler = new ActionHandler(redoAction);
+		((IHandlerService)getSite().getService(IHandlerService.class)).
+		activateHandler("com.onpositive.richtexteditor.snippets.browser.redo", (IHandler)redoHandler);
+	}
+
 	/**
 	 * Passing the focus request to the viewer's control.
 	 */
 	public void setFocus() {
-		viewer.getControl().setFocus();
+		richTextViewer.getControl().setFocus();
 	}
 }
