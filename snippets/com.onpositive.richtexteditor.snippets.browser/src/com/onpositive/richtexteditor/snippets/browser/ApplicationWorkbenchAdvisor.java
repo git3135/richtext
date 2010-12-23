e57855f2e53c672bdc8750dcd25b1e46efc919fc
dmitry.astu <dmitry.astu@gmail.com>
2010-12-23 14:44:02 +0000

    update delay, workbench settings

--- snippets/com.onpositive.richtexteditor.snippets.browser/src/com/onpositive/richtexteditor/snippets/browser/ApplicationWorkbenchAdvisor.java
+++ snippets/com.onpositive.richtexteditor.snippets.browser/src/com/onpositive/richtexteditor/snippets/browser/ApplicationWorkbenchAdvisor.java
@@ -1,5 +1,6 @@
 package com.onpositive.richtexteditor.snippets.browser;
 
+import org.eclipse.ui.application.IWorkbenchConfigurer;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchAdvisor;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
@@ -15,6 +16,14 @@ public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
 
 	public String getInitialWindowPerspectiveId() {
 		return PERSPECTIVE_ID;
+	}
+	
+	@Override
+	protected IWorkbenchConfigurer getWorkbenchConfigurer()
+	{
+		IWorkbenchConfigurer workbenchConfigurer = super.getWorkbenchConfigurer();
+		workbenchConfigurer.setSaveAndRestore(true);
+		return workbenchConfigurer;
 	}
 
 }
