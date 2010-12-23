3e1392be0b44d4bdd737dc37ece86f5d2af23fa5
dmitry.astu <dmitry.astu@gmail.com>
2010-12-23 13:07:36 +0000

    Initial commit

---
+++ snippets/com.onpositive.richtexteditor.snippets.browser/src/com/onpositive/richtexteditor/snippets/browser/View.java
@@ -0,0 +1,99 @@
+package com.onpositive.richtexteditor.snippets.browser;
+
+import org.eclipse.jface.viewers.IStructuredContentProvider;
+import org.eclipse.jface.viewers.ITableLabelProvider;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.ISharedImages;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.part.ViewPart;
+
+import com.onpositive.richtext.model.IRichDocumentListener;
+import com.onpositive.richtext.model.RichDocumentChange;
+import com.onpositive.richtext.model.meta.DocumentEvent;
+import com.onpositive.richtexteditor.viewer.RichTextViewer;
+
+public class View extends ViewPart {
+	public static final String ID = "com.onpositive.richtexteditor.snippets.browser.view";
+	private static final int MIN_UPDATE_INTERVAL = 2000; //2 sec
+
+	private RichTextViewer viewer;
+	private long lastUpdateTime = -1;
+	private Boolean updateScheduled = false;
+	
+
+
+	/**
+	 * This is a callback that will allow us to create the viewer and initialize
+	 * it.
+	 */
+	public void createPartControl(Composite parent) {
+		
+		parent.setLayout(new GridLayout(2,true));
+		viewer = new RichTextViewer(parent, SWT.BORDER);
+		GridData gridData = new GridData(GridData.FILL_BOTH);
+		gridData.horizontalSpan = 1;
+		gridData.minimumWidth = 200;
+		gridData.minimumHeight = 400;
+		viewer.getControl().setLayoutData(gridData);		
+		final Browser browser = new Browser(parent, SWT.BORDER );
+		GridData gridData1 = new GridData(GridData.FILL_BOTH);
+		gridData1.horizontalSpan = 1;
+		gridData1.minimumWidth = 200;
+		gridData1.minimumHeight = 400;
+		browser.setLayoutData(gridData1);				
+		viewer.addRichDocumentListener(new IRichDocumentListener(){
+
+			public void documentAboutToBeChanged(DocumentEvent event) {				
+			}
+
+			public void documentChanged(DocumentEvent event,
+					RichDocumentChange change) {
+				long current = System.currentTimeMillis();
+				synchronized (updateScheduled)
+				{
+					if (!updateScheduled)
+					{
+						if (lastUpdateTime == -1 || current - lastUpdateTime < MIN_UPDATE_INTERVAL)
+						{
+							String s =viewer.getLayerManager().getSerializedString();
+							browser.setText(s);
+							lastUpdateTime = current;
+						}
+						else
+						{
+							Display.getDefault().timerExec(MIN_UPDATE_INTERVAL, new Runnable()
+							{
+								
+								@Override
+								public void run()
+								{
+									String s = viewer.getLayerManager().getSerializedString();
+									browser.setText(s);
+									updateScheduled = false;
+									lastUpdateTime = System.currentTimeMillis();
+								}
+								
+							});
+							updateScheduled = true;
+						}
+					}
+				}
+			}
+		});		
+	}
+
+	/**
+	 * Passing the focus request to the viewer's control.
+	 */
+	public void setFocus() {
+		viewer.getControl().setFocus();
+	}
+}
