acd5569291673e0edb8ca6c25817440705f11b02
dmitry.astu@gmail.com <dmitry.astu@gmail.com@0b333480-fe57-fea6-b96e-3b5c4f84aef2>
2010-12-23 13:07:36 +0000

    Initial commit

---
+++ trunk/snippets/com.onpositive.richtexteditor.snippets.browser/src/com/onpositive/richtexteditor/snippets/browser/Perspective.java
@@ -0,0 +1,14 @@
+package com.onpositive.richtexteditor.snippets.browser;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class Perspective implements IPerspectiveFactory {
+
+	public void createInitialLayout(IPageLayout layout) {
+		layout.setEditorAreaVisible(false);
+		layout.setFixed(true);
+		
+	}
+
+}
